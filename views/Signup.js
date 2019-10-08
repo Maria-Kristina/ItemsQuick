2e97557b6b824cbd90e326eab71d98182bc8a97e
Oona <oona.e.rauhala@gmail.com>
2019-10-08 10:43:42 +0300

    Add username check functionality

--- views/Signup.js
+++ views/Signup.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import {Container, Content, Text, Header, Button} from 'native-base';
 import FormTextInput from '../components/FormTextInput';
-import useSignUpForm from '../hooks/SignUpHooks'
+import useSignUpForm from '../hooks/SignUpHooks';
+import {AsyncStorage} from 'react-native';
 
 
 const Signup = (props) => {
     const {inputs, handleUsernameChange, handlePasswordChange, handleEmailChange} = useSignUpForm();
 
+    const signUpAsync = async (url, data) => {
+        const url2 = url+'username/'+inputs.username;
+        console.log('Url checked: ', url2);
+        const response = await fetch(url2);
+        const json = await response.json();
+        console.log('Checkusername answer: ', json);
+
+        if (json.available == true) {
+            console.log('true, next posting to server');
+            const response = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data),
+            });
+            const json = await response.json();
+            console.log('Answer: ', json);
+        } else {
+            console.log('false');
+            
+        };
+        
+    };
+
+    const  hdcbhjb = async (url, data) => {
+        
+        
+        //await AsyncStorage.setItem('userToken', json.token);
+        // setting user data to asyncStorege
+        //await AsyncStorage.setItem('user', JSON.stringify(json.user));
+        //props.navigation.navigate('Home');
+      };
+
     const signUpButton = () => {
-        //Sign up post here
+        signUpAsync('http://media.mw.metropolia.fi/wbma/users/', inputs);
     };
 
     return(
